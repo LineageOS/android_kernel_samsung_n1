11a0ebb2357a01fbb5f44e58d7c679cc34b18d47
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2013-03-09 16:03:22 +0100

    ums: allow multiple lun files for n1 and bose
    independant part of CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
    
    Change-Id: I8fafe4565bb5be1971f6c5a57c3b1052838cd711

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -566,13 +566,19 @@ struct mass_storage_function_config {
 	struct fsg_common *common;
 };
 
+#if defined(CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE)
+# define UMS_WITH_MULTIPLE_LUNS
+#elif defined(CONFIG_MACH_N1) || defined(CONFIG_MACH_BOSE_ATT)
+# define UMS_WITH_MULTIPLE_LUNS
+#endif
+
 static int mass_storage_function_init(struct android_usb_function *f,
 					struct usb_composite_dev *cdev)
 {
 	struct mass_storage_function_config *config;
 	struct fsg_common *common;
 	int err;
-#ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
+#ifdef UMS_WITH_MULTIPLE_LUNS
 	int i;
 #endif
 	config = kzalloc(sizeof(struct mass_storage_function_config),
@@ -580,11 +586,11 @@ static int mass_storage_function_init(struct android_usb_function *f,
 	if (!config)
 		return -ENOMEM;
 
-#ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
+#ifdef UMS_WITH_MULTIPLE_LUNS
 	if (config) {
 		/* All of Nvidia device have SD card slot so we make 2 luns */
 		config->fsg.nluns = 2;
-		printk(KERN_DEBUG "usb: %s, nluns=%d\n", __func__,
+		pr_notice("usb: %s, nluns=%d\n", __func__,
 				config->fsg.nluns);
 
 		for (i = 0; i < config->fsg.nluns; i++) {
@@ -618,7 +624,7 @@ static int mass_storage_function_init(struct android_usb_function *f,
 	} else {
 #endif
 		/* original mainline code */
-		printk(KERN_DEBUG "usb: %s pdata is not available. nluns=1\n",
+		pr_notice("usb: %s pdata is not available. nluns=1\n",
 				__func__);
 		config->fsg.nluns = 1;
 		config->fsg.luns[0].removable = 1;
@@ -636,7 +642,7 @@ static int mass_storage_function_init(struct android_usb_function *f,
 			kfree(config);
 			return err;
 		}
-#ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
+#ifdef UMS_WITH_MULTIPLE_LUNS
 	}
 #endif
 	config->common = common;
